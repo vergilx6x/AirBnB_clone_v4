cd8778685df7e896cbb3d5ec0757f5894db07dca
vergilx6x <med.work96@hotmail.com>
2024-08-11 18:29:15 +0000

    Task 5

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -70,3 +70,46 @@ $.ajax({
     }
   }
 });
+
+$('button').on('click', function () {
+    const amIdList = [];
+    for (const id of Object.keys(checkedAmenities)) {
+      amIdList.push(id);
+    }
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:5001/api/v1/places_search',
+      data: `{"amenities": ${JSON.stringify(amIdList)}}`,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      success: function (data) {
+        $('section.places').empty();
+        console.log(data);
+        data.sort((a, b) => a.name.localeCompare(b.name));
+        for (const result of data) {
+          const pluralGuest = result.max_guest > 1 ? 's' : '';
+          const pluralBedroom = result.number_rooms > 1 ? 's' : '';
+          const pluralBathroom = result.number_bathrooms > 1 ? 's' : '';
+          $('section.places').append(`<article>
+          <div class="title_box">
+            <h2>${result.name}</h2>
+            <div class="price_by_night">$${result.price_by_night}</div>
+          </div>
+          <div class="information">
+            <div class="max_guest">${result.max_guest} Guest${pluralGuest}</div>
+            <div class="number_rooms">${result.number_rooms} Bedroom${pluralBedroom}</div>
+            <div class="number_bathrooms">${result.number_bathrooms} Bathroom${pluralBathroom}</div>
+          </div>
+          <div class="user">
+            <b>Owner:</b> TBD
+          </div>
+          <div class="description">
+            ${result.description}
+          </div>
+        </article>`);
+        }
+      }
+    });
+  });
+});
